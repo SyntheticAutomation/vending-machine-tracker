2de6fad53ee141c6e81cd5d1807b296d1dbf91dc
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-21 10:15:31 -0700

    Adds feature test to for user_story_1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,15 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+  it 'shows snacks and price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    machine_1 = owner.machines.create(location: "Harrisburg")
+    snack_1 = machine_1.snacks.create(name: "Fried Tarantula", price: 500)
+    snack_2 = machine_1.snacks.create(name: "Crab Cream au Gratin", price: 100)
+    snack_3 = machine_1.snacks.create(name: "Smores-Covered Bacon Mustard Lettuce Wraps", price: 3)
+
+    visit machine_path(machine_1)
+    expect(page).to have_content(snack_1.name)
+    expect(page).to have_content(snack_1.price)
+  end
 end
