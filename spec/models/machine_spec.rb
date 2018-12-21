a677acc0672cbcab138430206eed9a295870ab1f
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-21 10:34:13 -0700

    Adds model and feature tests for user story 2

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
-describe Owner, type: :model do
+describe Machine, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :name }
-    it { should have_many :machines }
+    it { should validate_presence_of :location }
+    it { should belong_to :owner }
+  end
+  describe 'instance methods' do
+    it '.average_snack_price' do
+      owner = Owner.create(name: "Sam's Snacks")
+      machine_1 = owner.machines.create(location: "Harrisburg")
+      snack_1 = machine_1.snacks.create(name: "Fried Tarantula", price: 500)
+      snack_2 = machine_1.snacks.create(name: "Crab Cream au Gratin", price: 100)
+      expect(machine_1.average_snack_price).to eq(300)
+    end
   end
 end
