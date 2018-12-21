b01043ca5dce51e56dc987619d9742fc787f734a
SyntheticAutomation <mclampet@msudenver.edu>
2018-12-21 10:34:24 -0700

    Adds code to complete user story 2 and passes all tests.

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,4 +2,10 @@ class Machine < ApplicationRecord
   validates_presence_of :location
   has_many :snacks
   belongs_to :owner
+
+
+  def average_snack_price
+    snacks.average(:price)
+  end
+
 end
